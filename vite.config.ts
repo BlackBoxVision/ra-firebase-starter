4273acfe3664a39e9d2db1ba5743261c718baa90
Manuel Tuero <manuel.tuero@blackbox-vision.tech>
2024-05-18 10:46:59 -0300

    fix: add missing path aliasing support and package name prefix

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import viteTsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    define: {
-        'process.env': process.env,
-    },
-    server: {
-        host: true,
-    },
-    base: './',
+  plugins: [react(), viteTsconfigPaths()],
+  server: {
+    // this ensures that the browser opens upon server start
+    open: true,
+    // this sets a default port to 3000
+    port: 3000,
+  },
+  base: "./",
 });
